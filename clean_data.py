2bab68e7ec797faa76fb5e00118ab1b37cc5645a
Shuang <34317191+ShuangWei1227@users.noreply.github.com>
2018-08-14 14:16:06 +0100

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -11,20 +11,27 @@ stemmer = SnowballStemmer('english')  # why ?
 stop_words = set(stopwords.words("english"))   # set()
 
 # read raw file:  -> DataFrame
-train_set = pd.read_csv("./survey2.csv")
+#train_set = pd.read_csv("./survey1_constructive.csv")
+train_set = pd.read_csv("./survey1_constructive.csv")
 #train_set.head()
 
 # get the 'comment column':
 #train_text = train_set["comment_text"]
-train_text = train_set.loc[:,"S2-Structure"]
+train_text = train_set.loc[:,"S1-Structure"]
+#train_text = train_set.loc[:,"S1-Delivery"]
+#train_text = train_set.loc[:,"S1-VisualAids"]
+#train_text = train_set.loc[:,"S2-Structure"]
 #train_text = train_set.loc[:,"S2-Delivery"]
 #train_text = train_set.loc[:,"S2-VisualAids"]
 
 
 
-train_array = train_set["S2-Structure"].values  # -> ndarray  (159571,)
-#train_array = train_set["S2-Delivery"].values  # -> ndarray  (159571,)
-#train_array = train_set["S2-VisualAids"].values  # -> ndarray  (159571,)
+train_array = train_set["S1-Structure"].values  # -> ndarray  (151,)
+#train_array = train_set["S1-Delivery"].values  # -> ndarray  (151,)
+#train_array = train_set["S1-VisualAids"].values  # -> ndarray  (151,)
+#train_array = train_set["S2-Structure"].values  # -> ndarray  (129,)
+#train_array = train_set["S2-Delivery"].values  # -> ndarray  (129,)
+#train_array = train_set["S2-VisualAids"].values  # -> ndarray  (129,)
 # A list to store cleaned text
 train_cleaned_comment_list = [] 
 
@@ -36,13 +43,15 @@ def clean_comment(comment):
         if _ == '\n':
             pass
         else:
+            #replace the characters which are not a-z or A-Z by a space " "
             re_train = str(re.sub("[^a-zA-Z]"," ", str(comment)))
             #make words in lower case
             tokened_train = word_tokenize(re_train.lower())
             #reserve the words which are in tokenen_train but not in stop_words
-            #stoped_train = [_ for _ in tokened_train if _ not in stop_words]
+            stoped_train = [_ for _ in tokened_train if _ not in stop_words]
             #integrate the words by using ' '
-            stoped_train_str = " ".join(tokened_train) # -> string
+            #stoped_train_str = " ".join(tokened_train) # -> string
+            stoped_train_str = " ".join(stoped_train) # -> string
             #return the results of cleaned data
             return stoped_train_str
 
@@ -52,7 +61,14 @@ Input cleaned data into lists and files
 '''
 # writelines to filess
 count = 0
-my_file = open('./cleaned_data/cleaned_survey2.csv','w')
+
+my_file = open('./cleaned_surveydata/cleaned_survey1_structure_new.csv','w')
+#my_file = open('./cleaned_surveydata/cleaned_survey1_delivery_new.csv','w')
+#my_file = open('./cleaned_surveydata/cleaned_survey1_visualaids_new.csv','w')
+#my_file = open('./cleaned_surveydata/cleaned_survey2_structure_new.csv','w')
+#my_file = open('./cleaned_surveydata/cleaned_survey2_delivery_new.csv','w')
+#my_file = open('./cleaned_surveydata/cleaned_survey2_visualaids_new.csv','w')
+
 for _ in train_array:
     i = clean_comment(_)
     print(i)
